d7573a4a5d4c21e85272d96fffbd2e9517d4f2c6
1Noah1 <noah.storck@me.com>
2022-11-05 22:12:42 +0100

    removed two player code lines

--- customGoldenPotGame/Program.cs
+++ customGoldenPotGame/Program.cs
@@ -53,24 +53,24 @@ namespace customGoldenPotGame
             }
         }
 
-        public class Playables {
+        public class Playable
+        {
 
             // read char input properly
             ConsoleKeyInfo keyInfo = new ConsoleKeyInfo();
 
-            //array for saving Playables pos
+            //array for saving Playable pos
             public int[] mainPlayablePos = new int[2];
-            public int[] secondPlayablePos = new int[2];
             public int[] mainPlayablePosHistory = new int[2];
 
             // Use Width and height from map instead
             public int[] boundaries = new int[4];
 
-            
+
             public int x { get; set; }
             public int y { get; set; }
 
-            public Playables()
+            public Playable()
             {
                 // main Playable start Pos
                 x = 30;
@@ -79,10 +79,8 @@ namespace customGoldenPotGame
                 mainPlayablePos[0] = x;
                 mainPlayablePos[1] = y;
 
-                secondPlayablePos[0] = x;
-                // second Player offset is 20
-                secondPlayablePos[1] = y + 20;
-
+                // these values should not have individual ints
+                // they should just reference the Widht and Height vars from the Map class
                 // maybe order these values differently (by wasd system) 
                 //border right
                 boundaries[0] = 65;
@@ -92,63 +90,24 @@ namespace customGoldenPotGame
                 boundaries[2] = 32;
                 //border top
                 boundaries[3] = 4;
-
             }
-            public void initializePlayables(bool mainPlayable)
+            public void initializeCharacter()
             {
-                bool outOfBounds = false;
-                if (mainPlayable)
-                {
-                    // Cursor set to modified Pos
-                    if (mainPlayablePos[0] > boundaries[0] || mainPlayablePos[0] < boundaries[1])
-                    {
-                        outOfBounds = true;
-                    }else if (mainPlayablePos[1] > boundaries[2] || mainPlayablePos[1] < boundaries[3])
-                    {
-                        outOfBounds = true;
+                // Set Cursor to next Pos 
+                Console.SetCursorPosition(mainPlayablePos[0], mainPlayablePos[1]);
+                // maybe make character out of multiple lines of chars
+                // Character Symbol
+                // the @ character causes weird bug to appear idk why
+                Console.Write("0");
 
-                    }
-                    if (!outOfBounds)
-                    {
-                        // Set Cursor to next Pos 
-                        Console.SetCursorPosition(mainPlayablePos[0], mainPlayablePos[1]);
-                        // maybe make character out of multiple lines of chars
-                        // Character Symbol
-                        // main playable
-                        Console.Write("O");
+                // Set Cursor to last Pos
+                Console.SetCursorPosition(mainPlayablePosHistory[0], mainPlayablePosHistory[1]);
 
-                        // Set Cursor to last Pos
-                        Console.SetCursorPosition(mainPlayablePosHistory[0], mainPlayablePosHistory[1]);
+                // erase line by overwriting it with empty character
+                Console.Write(" ");
 
-                        // erase line by overwriting it with empty character
-                        Console.Write(" ");
-
-
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.SetCursorPosition(30, 18);
-                        Console.Write("You left the PLayground");
-                    }
-
-
-                }
-                else
-                {
-                    // Cursor set to modified Pos
-                    Console.SetCursorPosition(secondPlayablePos[0], secondPlayablePos[1]);
-
-                    // maybe make character out of multiple lines of chars
-                    // Character Symbol
-                    // second playable
-                    Console.Write("X");
-                    //Console.Write('^');
-                }
- 
             }
 
-        
             public void movement()
             {
                 // gets user Input 
@@ -159,18 +118,19 @@ namespace customGoldenPotGame
                 mainPlayablePosHistory[0] = mainPlayablePos[0];
                 mainPlayablePosHistory[1] = mainPlayablePos[1];
 
-                if(inputKey == 'w')
+                if (inputKey == 'w')
                 {
                     //up
                     // check for avoiding going out of bounds
                     // not the smartest solution but it works (optimize)
                     // flickering appears because trail is removed while not changing position
                     //on key hold char dissapears until you move the opposite direction
-                    if (mainPlayablePos[1] > boundaries[3]) {
+                    if (mainPlayablePos[1] > boundaries[3])
+                    {
                         mainPlayablePos[1]--;
                     }
-
-                }else if (inputKey == 'a')
+                }
+                else if (inputKey == 'a')
                 {
                     //left
                     if (mainPlayablePos[0] > boundaries[1])
@@ -183,9 +143,10 @@ namespace customGoldenPotGame
                     //down
                     if (mainPlayablePos[1] < boundaries[2])
                     {
-                       mainPlayablePos[1]++;
+                        mainPlayablePos[1]++;
                     }
-                }else if (inputKey == 'd')
+                }
+                else if (inputKey == 'd')
                 {
                     if (mainPlayablePos[0] < boundaries[0])
                     {
@@ -193,7 +154,7 @@ namespace customGoldenPotGame
                         mainPlayablePos[0]++;
                     }
                 }
-                Thread.Sleep(100);       
+                Thread.Sleep(100);
             }
         }
 
@@ -236,21 +197,20 @@ namespace customGoldenPotGame
 
             bool exit = false;
             Map map = new Map();
-            Playables Playables = new Playables();
-            //Playables Playables = new Playables();
+            Playable Playable = new Playable();
+            //Playable Playable = new Playable();
             // decreased Bufferheight for performance
-                //probably doesn't affect performace drastically
+            //probably doesn't affect performace drastically
             Console.BufferHeight = 50;
             while (!exit)
             {
                 map.renderMap();
                 //main Character is standard Character
-                bool mainPlayable = true;
                 bool failed = false;
                 while (!failed)
                 {
-                    Playables.initializePlayables(mainPlayable);
-                    Playables.movement();
+                    Playable.initializeCharacter();
+                    Playable.movement();
                 }
                 //for debugging/development
                 //Thread.Sleep(20000);     
